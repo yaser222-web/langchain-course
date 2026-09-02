534d8ab36133c09e712a8439087e738c32a0d382
yaser222-web <70367587+yaser222-web@users.noreply.github.com>
2026-09-02 21:46:30 +0330

    Read the PDF and make summarize

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from openai import OpenAI
 from langchain_openai import ChatOpenAI
 from langchain_core.prompts import PromptTemplate
 from dotenv import load_dotenv
+from langchain_community.document_loaders import PyPDFLoader
 import os
 load_dotenv()
 
@@ -15,18 +16,13 @@ load_dotenv()
 
 def main():
     print("Hello from langchain-course!")
-    information = """ Elon Reeve Musk (/ˈiːlɒn/ ⓘ EE-lon; born June 28, 1971) is a businessman and former public official who is the CEO and largest shareholder of Tesla and SpaceX. Musk has been the wealthiest person in the world since 2025, and briefly became the only trillionaire (in terms of US dollars) in June 2026; as of August 14, 2026, Forbes estimates his net worth to be US$864 billion.
-
-    Born into the wealthy Musk family in Pretoria, South Africa, Musk emigrated in 1989 to Canada; he has Canadian citizenship since his mother was born there. He received bachelor's degrees in 1997 from the University of Pennsylvania before moving to California to pursue business ventures. In 1995, Musk co-founded Zip2, a web software company. Following its sale in 1999, he co-founded X.com, an e-commerce payment system that merged with Confinity in March 2000 to form PayPal, which was acquired by eBay in 2002. Musk also became an American citizen in 2002.
-
-    In 2002, Musk founded and became CEO and chief engineer of SpaceX, a space technology company; the company has since led innovations in reusable rockets and commercial spaceflight. Musk joined Tesla as an early investor in 2004 and became its CEO and product architect in 2008; it has since become a leader in electric vehicles. In 2015, Musk co-founded OpenAI to advance artificial intelligence (AI) research, but later left; his growing discontent with the organization's direction and leadership in the AI boom in the 2020s led him to establish xAI, which became a subsidiary of SpaceX in 2026. In 2022, he acquired Twitter, a social networking service, and made significant changes, including rebranding it as X in 2023. His other businesses include Neuralink, a neurotechnology company that he co-founded in 2016, and the Boring Company, a tunneling company that he founded in 2017. In November 2025, Tesla approved a pay package worth $1 trillion for Musk, which he is to receive over 10 years if certain milestones are met, such as achieving a market capitalization of $8.5 trillion. Musk became the first US-dollar trillionaire upon the initial public offering of SpaceX in June 2026, though a SpaceX stock collapse brought him under the threshold by the next month.
-
-    Musk was the largest donor in the 2024 U.S. presidential election, where he supported Donald Trump. After Trump was inaugurated in January 2025, Musk was Senior Advisor to the President and the de facto head of the Department of Government Efficiency (DOGE). Musk left the Trump administration in May 2025 and returned to managing his companies; shortly thereafter he had a public feud with Trump."""
-
+    loader = PyPDFLoader(r"C:\Users\Yaser\Downloads\Documents\example.pdf")
+    pages = loader.load()
+    information = "\n".join(page.page_content for page in pages)
     summary_template = """
-    given the information {information} about a person I want you to create:
-    1. A short summary
-    2.two interesting facts about them """ 
+    Summarize the following document: {information}
+    Give me:
+    the most important points """ 
 
     summary_prompt_template = PromptTemplate(input_variables= ["information"], template = summary_template)
     llm = ChatOpenAI(temperature=0, model="gpt-5.6-luna",api_key=os.getenv("OPENAI_API_KEY"))
